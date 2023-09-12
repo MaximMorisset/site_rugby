5969c2f9bf97e37de1c2a4be16046efd6c2f18a7
MaximMorisset <morisset.maxim.pro@gmail.com>
2023-09-12 17:05:49 +0200

    Premier commit

---
+++ src/Entity/Equipe.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipeRepository::class)]
+class Equipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom_equipe = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEquipe(): ?string
+    {
+        return $this->nom_equipe;
+    }
+
+    public function setNomEquipe(string $nom_equipe): static
+    {
+        $this->nom_equipe = $nom_equipe;
+
+        return $this;
+    }
+}
