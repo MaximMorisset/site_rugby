b59e1649d3d73df0516fe52308f4f155f4c927ff
MaximMorisset <morisset.maxim.pro@gmail.com>
2023-10-10 11:49:52 +0200

    Club

---
+++ src/Controller/ClubController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Club;
+use App\Form\ClubType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/club.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+    
+    #[Route('/add/club', name: 'app_create_club')]
+    public function createClub(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $club = new Club();
+        $form = $this->createForm(ClubType::class, $club);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($club);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+        }
+        return $this->render('club/create_club.html.twig', [
+            'clubForm' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/ClubType.php
+++ src/Form/ClubType.php
@@ -6,14 +6,36 @@ use App\Entity\Club;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ClubType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('terrain')
+            ->add('nom', TextType::class, [
+                'label'=> 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez renseignez votre Nom'
+                    ])
+                ]
+            ])
+            ->add('terrains', TextType::class, [
+                'label'=> 'Adresse du Terrain', 
+                'attr' => [
+                    'placeholder' => 'Adresse du Terrain'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message'=> "Veuillez saisir l'adresse du terrain" 
+                    ])
+                ]
+            ])
         ;
     }
 
